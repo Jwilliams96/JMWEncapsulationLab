a20030bd2f6c1cf1723b8164825030c68888b359
Jwilliams96 <Jwilliams96@L11119038628.wctc.msft>
2012-08-30 14:29:59 -0500

    Finished lab 1

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -3,36 +3,36 @@ package lab1;
 import java.util.Date;
 
 /**
- * In this lab your challenge is to fix the code in both classes to use
- * proper encapsulation and the four other best practices as explained by 
- * your instructor.
+ * In this lab your challenge is to fix the code in both classes to use proper
+ * encapsulation and the four other best practices as explained by your
+ * instructor.
  *
- * @author      Jim Lombardo, WCTC Instructor
- * @version     1.01
+ * @author Jim Lombardo, WCTC Instructor
+ * @version 1.01
  */
 public class Employee {
-    String firstName;
-    String lastName;
-    public String ssn;
-    public Date birthDate;
-    boolean metWithHr;
-    boolean metDeptStaff;
-    boolean reviewedDeptPolicies;
-    boolean movedIn;
-    String cubeId;
+
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private Date birthDate;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private String cubeId;
 
     public Employee() {
-
     }
 
     // Assume this must be performed first
-    public void meetWithHrForBenefitAndSalryInfo() {
+    private void meetWithHrForBenefitAndSalryInfo() {
         metWithHr = true;
     }
 
     // Assume this is must be performed second
-    public void meetDepartmentStaff() {
-        if(metWithHr) {
+    private void meetDepartmentStaff() {
+        if (metWithHr) {
             metDeptStaff = true;
         } else {
             throw new IllegalStateException("Sorry, you cannot meet with "
@@ -41,8 +41,8 @@ public class Employee {
     }
 
     // Assume this must be performed third
-    public void reviewDeptPolicies() {
-        if(metWithHr && metDeptStaff) {
+    private void reviewDeptPolicies() {
+        if (metWithHr && metDeptStaff) {
             reviewedDeptPolicies = true;
         } else {
             throw new IllegalStateException("Sorry, you cannot review "
@@ -51,9 +51,20 @@ public class Employee {
         }
     }
 
-    // Assume this must be performed 4th
+    /*
+     * I did not remove the extra status checks because i feel that if there 
+     * were to be extra criteria or requirements to be met in each step, then 
+     * need to have them in there: metWithHr, metDeptStaff, & 
+     * reviewedDeptPolicies are the ones that i did that with, and there 
+     * related methods.
+     */
     public void moveIntoCubicle(String cubeId) {
-        if(metWithHr && metDeptStaff && reviewedDeptPolicies) {
+
+        this.meetWithHrForBenefitAndSalryInfo();
+        this.meetDepartmentStaff();
+        this.reviewDeptPolicies();
+
+        if (metWithHr && metDeptStaff && reviewedDeptPolicies) {
             this.cubeId = cubeId;
             this.movedIn = true;
         } else {
@@ -65,12 +76,76 @@ public class Employee {
 
     }
 
+    /*
+     * i removed all the other checks, because there is no way to have this 
+     * check without the other ones.
+     */
     public String getStatus() {
-        if(metWithHr && metDeptStaff
-           && reviewedDeptPolicies && movedIn) {
+        if (movedIn) {
             return "Orientation is complete";
         } else {
             return "Orientation in progress...";
         }
+    }
+
+    //accessors for all properties & fields
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    /*
+     * mutators for all but the checks. they should not be manipulated by the
+     * main class. these should all have input validation as well.
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
     }
 }
--- src/lab1/Startup.java
+++ src/lab1/Startup.java
@@ -1,26 +1,26 @@
 package lab1;
 
 /**
- * In this lab your challenge is to fix the code in both classes to use
- * proper encapsulation and the four other best practices as explained by 
- * your instructor.
+ * In this lab your challenge is to fix the code in both classes to use proper
+ * encapsulation and the four other best practices as explained by your
+ * instructor.
  *
- * @author      Jim Lombardo, WCTC Instructor
- * @version     1.01
+ * @author Jim Lombardo, WCTC Instructor
+ * @version 1.01
  */
 public class Startup {
+
     public static void main(String[] args) {
         Employee employee = new Employee();
-        employee.firstName = "Peter";
-        employee.lastName = "Piper";
-        employee.ssn = "333-1234";
-        
-        employee.meetDepartmentStaff();
-        employee.meetWithHrForBenefitAndSalryInfo();
-        employee.reviewDeptPolicies();
+        employee.setFirstName("Peter");
+        employee.setLastName("Piper");
+        employee.setSsn("333-1234");
+
+        /*
+         * This is where you would ask for user input in the cubicle ID and 
+         * run the move into cubicle method.
+         */
 
         System.out.println("The employee's status is: " + employee.getStatus());
     }
-    
-    
 }
